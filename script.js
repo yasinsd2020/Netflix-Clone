aedb4a64b01ed0962b6e33868e58abf4aa14eeb1
yasinsd2020 <85822908+yasinsd2020@users.noreply.github.com>
2022-05-24 12:04:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+
+
+function myfunction1(){
+    let a = document.querySelector('.close1');
+    let b=document.querySelector('.artical-boX1');
+
+     if (a.style.display === "none"  || b.style.display === "none" ) {
+      a.style.display = "fleX";
+      b.style.display ="fleX";
+      
+    } else {
+      a.style.display = "none";
+      b.style.display ="none";
+    }
+  }
+function myfunction2(){
+    let c = document.querySelector('.close2');
+    let d=document.querySelector('.artical-boX2');
+
+     if (c.style.display === "none"  || d.style.display === "none" ) {
+      c.style.display = "fleX";
+      d.style.display ="fleX";
+      
+    } else {
+      c.style.display = "none";
+      d.style.display ="none";
+    }
+  }
+function myfunction3(){
+    let e = document.querySelector('.close3');
+    let f=document.querySelector('.artical-boX3');
+
+     if (e.style.display === "none"  || f.style.display === "none" ) {
+      e.style.display = "fleX";
+      f.style.display ="fleX";
+      
+    } else {
+      e.style.display = "none";
+      f.style.display ="none";
+    }
+  }
+function myfunction4(){
+    let e = document.querySelector('.close4');
+    let f=document.querySelector('.artical-boX4');
+
+     if (e.style.display === "none"  || f.style.display === "none" ) {
+      e.style.display = "fleX";
+      f.style.display ="fleX";
+      
+    } else {
+      e.style.display = "none";
+      f.style.display ="none";
+    }
+  }
+function myfunction5(){
+    let e = document.querySelector('.close5');
+    let f=document.querySelector('.artical-boX5');
+
+     if (e.style.display === "none"  || f.style.display === "none" ) {
+      e.style.display = "fleX";
+      f.style.display ="fleX";
+      
+    } else {
+      e.style.display = "none";
+      f.style.display ="none";
+    }
+  }
+function myfunction6(){
+    let e = document.querySelector('.close6');
+    let f=document.querySelector('.artical-boX6');
+
+     if (e.style.display === "none"  || f.style.display === "none" ) {
+      e.style.display = "fleX";
+      f.style.display ="fleX";
+      
+    } else {
+      e.style.display = "none";
+      f.style.display ="none";
+    }
+  }
+
+  // Video autoplay section
+  document.querySelector('video').play();
